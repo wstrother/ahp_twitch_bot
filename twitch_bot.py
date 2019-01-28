1d8595f22107979b524dfefaeb697b998150110e
wstrother <wstrother@gmail.com>
2019-01-28 14:55:04 -0500

    added documentation to twitch_chat.py and twitch_bot.py

--- twitch_bot.py
+++ twitch_bot.py
@@ -5,9 +5,34 @@ import commands
 import json
 import inspect
 
+#   The twitch_bot.py module defines a TwitchBot class that manages a set of
+# commands, listeners, and approved users that can use certain restricted
+# commands. The bot should have a valid Twitch account associated with it and
+# will need an oauth token associated with that account to log into chat
+# channels on the Twitch IRC server.
+#   The bot account should typically be given mod powers in the chat room it
+# is to be used in, to prevent the server from timing out from sending too many
+# messages too close together.
+
+#   The BotLoader class is also defined, which defines a JSON data interface
+# that allows for the programmatic creation of Command and Listener objects,
+# defined in the commands.py, and listeners.py modules respectively. The JSON
+# file provided can also provide a list of approved users who have access to
+# additional restricted bot commands.
+
 
 class TwitchBot:
     def __init__(self, user_name, token_file):
+        """
+        Creates an object representing a TwitchBot that allows for various
+        commands as well as listener routines that respond to certain trigger
+        strings within the chat. Also contains a list of user names for approved
+        users who can use special restricted commands
+
+        :param user_name: str, user name of a valid Twitch account
+        :param token_file: str, file name cotaining a valid oauth
+            token for that account
+        """
         self.user_name = user_name
         self.token_file = token_file
         self.chat = None
@@ -18,6 +43,20 @@ class TwitchBot:
         self.listeners = []
 
     def run(self, channel, join_msg):
+        """
+        Method creates a TwitchChat object to establish a connection
+        to the Twitch IRC server and then joins a specific channel,
+        representing the Twitch chat room of a specific user
+
+        Upon successfully connecting to the server and joining the
+        channel, this method runs a loop of the TwitchChat.update_chat()
+        method, which in turn passes messages from the chat to the
+        TwitchBot's 'handle_message()' method
+
+        :param channel: str, name of the Twitch chat to join
+        :param join_msg: str, optional chat messsage to send after
+            successfully joining the chat room
+        """
         host_name = channel[1:]
         self.approved_users.append(host_name)
 
@@ -32,10 +71,23 @@ class TwitchBot:
             self.chat.update_chat()
 
     def send_chat(self, message):
+        """
+        Sends a message to the Twitch chat
+
+        :param message: str, message for chat
+        """
         self.chat.send_chat(message)
 
     def handle_message(self, user, msg):
+        """
+        Parses messages sent to the chat to detect commands
+        and pass to any active chat listeners.
+
+        :param user: str, name of user who sent message
+        :param msg: str, message sent to chat
+        """
         msg = msg.replace("\r", "")
+        msg = msg.replace("\n", "")
 
         if msg[0] == "!":
             msg = msg.split(" ")
@@ -49,27 +101,78 @@ class TwitchBot:
                 listener.hear_message(user, msg)
 
     def do_command(self, command, user, *args):
+        """
+        Checks for command name in commands dict and passes
+        the user and args to the 'do' method of the associated
+        command object
+
+        :param command: str, command name
+        :param user: str, name of user that used command
+        :param args: tuple(str, str...), generic arguments exploded by
+            space characters chat message after the command "!name" str
+        """
         if command in self.commands:
             self.commands[command].do(user, *args)
 
     def add_command(self, cls, *args):
+        """
+        Instantiates a command object from a class and args then
+        adds that command to the commands dict under the key of
+        the command.name attribute
+
+        :param cls: Command class or subclass
+        :param args: initialization args for Command class
+        """
         command = cls(self, *args)
         self.commands[command.name] = command
 
     def set_commands(self, *coms):
+        """
+        This method takes an arbitrary amount of argument entries,
+        each taking the form of some iterable to be passed as
+        arguments to 'add_command()'
+            (Command class or subclass, *args)
+
+        :param coms: sets of arguments for 'add_command'
+            method.
+        """
         for c in coms:
             self.add_command(*c)
 
-    def set_state(self, key, value):
+    def set_state(self, key, value, log=True):
+        """
+        Alters the 'state' dict to store a variable
+
+        :param key: str, name of the state variable
+        :param value: any value to be stored as variable
+        :param log: bool, determines if a log message should
+            be sent to the Twitch chat
+        """
         self.state[key] = value
-        self.send_chat("variable '{}' set to '{}'".format(
-            key, value
-        ))
+        if log:
+            self.send_chat("variable '{}' set to '{}'".format(
+                key, value
+            ))
 
     def get_state_variable(self, key):
+        """
+        Returns the value of some state variable stored
+        in 'state' dict under the 'key' name
+
+        :param key: str, name of state variable
+        :return: value of the state variable, or None
+        """
         return self.state.get(key)
 
     def add_listener(self, listener):
+        """
+        Adds some chat listener to TwitchBot's list of active
+        listeners if that listener hasn't already been added.
+
+        Prints a log message to the console when listener is added
+
+        :param listener: Listener object
+        """
         if listener not in self.listeners:
             self.listeners.append(listener)
 
@@ -78,6 +181,11 @@ class TwitchBot:
             ))
 
     def remove_listener(self, listener):
+        """
+        Removes listener from TwitchBot's list of active listeners
+
+        :param listener: Listener object
+        """
         if listener in self.listeners:
             self.listeners.pop(
                 self.listeners.index(listener)
@@ -85,10 +193,21 @@ class TwitchBot:
 
 
 class BotLoader:
-    def __init__(self, file_name, bot_class, user_name, token_file, *args):
-        self.bot = self.make_bot(bot_class, user_name, token_file, *args)
+    # key names for JSON data
+    APPROVED_USERS = "approved_users"   # users with access to restricted commands
+    CLASSES = "classes"                 # class name aliases
+    RESTRICTED = "restricted"           # restricted commands
+    PUBLIC = "public"                   # unrestricted commands
 
-        file = open(file_name, "r")
+    def __init__(self, json_file, cd=None):
+        """
+        Creates a loader class the provides an interface to set a Twitch bot's
+        attributes, commands, and listeners based on JSON data
+
+        :param json_file: str, file_name for JSON data
+        :param cd: dict, optional dict to provide additional classes
+        """
+        file = open(json_file, "r")
         self.json = json.load(file)
         file.close()
 
@@ -99,24 +218,65 @@ class BotLoader:
             c[0]: c[1] for c in inspect.getmembers(listeners) if inspect.isclass(c[1])
         })
 
-    @staticmethod
-    def load_bot(json_file, bot_class, user_name, token_file, *args):
-        loader = BotLoader(
-            json_file, bot_class, user_name, token_file, *args
+        if cd:
+            self.class_dict.update(cd)
+
+    @classmethod
+    def load_bot(cls, json_file, bot_class, bot_args, cd=None):
+        """
+        Method that creates a BotLoader object and instantiates
+        a bot object from the 'bot_class' passed.
+
+        The loader then sets attributes and
+
+        :param json_file: str, file_name passed to __init__
+        :param bot_class: TwitchBot or subclass used to instantiate bot
+        :param bot_args: iterable of args passed to bot_class.__init__
+        :param cd: dict, optional class dict passed to __init__
+
+        :return: bot object with attributes, commands, listeners set
+            according to JSON data
+        """
+        loader = cls(
+            json_file, cd=cd
         )
-        loader.add_approved_users()
-        loader.set_commands()
 
-        return loader.bot
+        bot = bot_class(*bot_args)
+        loader.set_attributes(bot)
+        loader.set_commands(bot)
 
-    @staticmethod
-    def make_bot(bot_class, user_name, token_file, *args):
-        return bot_class(user_name, token_file, *args)
+        return bot
 
-    def add_approved_users(self):
-        self.bot.approved_users += self.json["approved_users"]
+    def set_attributes(self, bot):
+        """
+        This method provides a subclass hook that defines the routine for
+        what bot attributes should be set using the JSON data
+
+        :param bot: bot object to have attributes set
+        """
+        self.add_approved_users(bot)
+
+    def add_approved_users(self, bot):
+        """
+        Adds user names for approved users to the bot object
+
+        :param bot: bot object to have approved_users set
+        """
+        bot.approved_users += self.json[
+            self.__class__.APPROVED_USERS
+        ]
 
     def get_class(self, key):
+        """
+        This method helps recursively substitute JSON data values
+        with Class objects if they match the name of any Class
+        objects found in the 'class_dict' or aliases thereof as
+        defined within the JSON data
+
+        :param key: str, potential class name or alias
+        :return: Class, if key corresponds to a Class or
+                 str, if key does not
+        """
         if type(key) in (tuple, list):
             return [self.get_class(i) for i in key]
 
@@ -126,7 +286,10 @@ class BotLoader:
                 return cd[key]
 
             else:
-                aliases = self.json["classes"]
+                aliases = self.json[
+                    self.__class__.CLASSES
+                ]
+
                 if key in aliases:
                     return cd[aliases[key]]
 
@@ -134,6 +297,21 @@ class BotLoader:
                     return key
 
     def get_command(self, restricted, entry):
+        """
+        This method takes a single 'entry' from either the 'public' or
+        'restricted' list as defined in the JSON data and parses it for
+        Class names and Class name aliases, substituting the associated
+        Class objects and returning a tuple that can be used as arguments
+        for the 'set_commands()' method
+
+        :param restricted: bool, passed to the Command object
+            on initialization
+        :param entry: tuple, generic args passed to the Command
+            object on initialization
+
+        :return: tuple, args to be passed to the bot object through
+            the 'set_commands()' method
+        """
         cls, *args = entry
         cls = self.get_class(cls)
 
@@ -150,27 +328,39 @@ class BotLoader:
 
         return tuple([cls, name, restricted] + args)
 
-    def set_commands(self):
+    def set_commands(self, bot):
+        """
+        Iterates over the data entries specified within the 'restricted'
+        and 'public' lists in the JSON data and generates a list of argument
+        tuples to be passed to the bot object's 'set_commands()' method
+
+        :param bot: bot object to have commands set
+        """
         entries = []
 
-        for entry in self.json["restricted"]:
+        for entry in self.json[
+            self.__class__.RESTRICTED
+        ]:
             entries.append(
                 self.get_command(True, entry)
             )
 
-        for entry in self.json["public"]:
+        for entry in self.json[
+            self.__class__.PUBLIC
+        ]:
             entries.append(
                 self.get_command(False, entry)
             )
 
-        self.bot.set_commands(*entries)
+        bot.set_commands(*entries)
 
 
 if __name__ == "__main__":
     USER_NAME = "ahp_helper_bot"
     CHANNEL = "#athenshorseparty420"
     TOKEN = "oauth.token"
+    SETTINGS = "bot_settings.json"
 
     BotLoader.load_bot(
-        "bot_settings.json", TwitchBot, USER_NAME, TOKEN
+        SETTINGS, TwitchBot, (USER_NAME, TOKEN)
     ).run(CHANNEL, "Logging on...")
--- twitch_chat.py
+++ twitch_chat.py
@@ -1,5 +1,19 @@
 from socket import socket
 
+#   The twitch_chat.py module defines a class called TwitchChat that represents a
+# persistent connection to a Twitch chat room, using the IRC protocol with the
+# socket module. To connect to Twitch's IRC server you will need:
+#       - A Twitch account
+#       - A valid oauth token for that account
+#           (available at tmi.twitch.com)
+#       - The name of a valid Twitch channel
+#   The TwitchChat class also accepts a "bot" object as an initialization argument
+# which it sets as an attribute and passes chat messages to that bot via the
+# 'handle_message(user, msg)' method.
+#   The Twitch account should ideally have moderator privileges on the channel that
+# it joins, to ensure that automated messages sent to the server don't get
+# flagged as spam or abuse which may result in the connection being timed out
+
 
 class TwitchChat:
     HOST = "irc.twitch.tv"
@@ -8,27 +22,58 @@ class TwitchChat:
     RECV_BUFFER_SIZE = 1024
 
     def __init__(self, user_name, token_file, channel, bot=None):
+        """
+        Returns a TwitchChat object and initializes a connection to the
+        'irc.twitch.tv' server via a socket object imported from the
+        socket module.
+
+        Messages from the server are stored as a list of string in the
+        'chat_buffer' attribute, with an default max buffer size of
+        1000 messages, to aid in debugging.
+
+        :param user_name: str, the name of a valid Twitch account
+        :param token_file: str, file name contain oauth token for the
+            connecting Twitch account
+        :param channel: str, the name of a valid Twitch channel on the
+            irc server (should take the form '#channelname'
+        :param bot: object, an instance of a TwitchBot or subclass,
+            designed to handle and respond to messages from the chat
+        """
         self.user_name = user_name
         self.token_file = token_file
         self.channel = channel
         self.bot = bot
+
         self.socket = self.get_socket()
         self.chat_buffer = []
 
     @property
     def chat_text(self):
+        """
+        Returns the full chat buffer as a string, to search for substrings
+        within any messages received from the server.
+
+        :return: str, a concatenation of the 'chat_buffer' attribute
+        """
         return "\n".join(self.chat_buffer)
 
     @classmethod
     def set_chat_buffer(cls, n):
+        """
+        Used to set the size of the 'chat_buffer' list at run time
+
+        :param n: int, new maximum buffer size
+        """
         cls.CHAT_BUFFER_SIZE = n
 
     @classmethod
-    def set_recv_buffer(cls, n):
-        cls.RECV_BUFFER_SIZE = n
-
-    @classmethod
     def get_socket(cls):
+        """
+        Used to open a connection to the server
+
+        :return: socket, socket module socket object with live
+            connection to the Twitch IRC server
+        """
         s = socket()
         s.connect((
             cls.HOST,
@@ -38,6 +83,12 @@ class TwitchChat:
         return s
 
     def get_password(self):
+        """
+        Reads the contents of the oauth token file and returns
+        as a string for logging onto the Twitch IRC server
+
+        :return: str, the oauth token for authentication
+        """
         file = open(self.token_file, "r")
         token = file.read()
         file.close()
@@ -45,18 +96,41 @@ class TwitchChat:
         return token
 
     def send(self, msg):
+        """
+        Properly formats and encodes a message to be sent
+        through the socket to the IRC server
+
+        :param msg: str, message for server
+        """
         msg += "\r\n"
         self.socket.send(
             msg.encode("utf-8")
         )
 
     def send_chat(self, msg):
+        """
+        Formats a message to be sent to the IRC server as a chat
+        message for the channel that is currently joined, i.e.
+        the Twitch chat
+
+        :param msg: str, message to be sent to the IRC channel
+        """
         self.send("PRIVMSG {} :{}".format(
             self.channel, msg
         ))
         self.print_chat_message(self.user_name, msg)
 
     def join_chat(self, join_msg=None):
+        """
+        Method called to join the specific channel on the IRC server
+        that represents the Twitch chat that is being joined.
+        Authenticates the user account and sends an optional join
+        message to the chat.
+
+        :param join_msg: str, optional message to be sent to
+            the Twitch chat upon successfully joining the channel
+        :return: TwitchChat object, returns copy of self
+        """
         self.send("PASS " + self.get_password())
         self.send("NICK " + self.user_name)
         self.send("JOIN " + self.channel)
@@ -73,21 +147,69 @@ class TwitchChat:
         return self
 
     def update_chat(self):
-        lines = self.chat_buffer
-
+        """
+        This method is called to check for new messages from the
+        IRC server and parse each message, passing them one 'line'
+        at a time to the 'handle_server_message(line)' method
+        """
         chat_text = self.socket.recv(
             self.RECV_BUFFER_SIZE
         ).decode("utf-8").split("\r\n")[:-1]
 
         for line in chat_text:
-            if not self.check_for_ping(line):
-                self.print_line(line)
+            self.handle_server_message(line)
+            self.update_buffer(line)
 
-            lines.append(line)
-            if len(lines) >= self.CHAT_BUFFER_SIZE:
-                lines.pop(0)
+    def update_buffer(self, line):
+        """
+        Updates the 'chat_buffer' attribute and limits the size to
+        the amount specified by CHAT_BUFFER_SIZE
+
+        :param line: str, message from server to be added to buffer
+        """
+        self.chat_buffer.append(line)
+        if len(self.chat_buffer) > self.CHAT_BUFFER_SIZE:
+            self.chat_buffer.pop(0)
+
+    def handle_server_message(self, line):
+        """
+        This method parses messages from the server and determines
+        whether they're an actual IRC server message or chat messages
+        for the specific channel.
+
+        Server messages are checked to see whether they are a PING
+        message so that the appropriate response can be sent to
+        prevent the connection from being timed out. Otherwise they
+        are simply printed to the console.
+
+        Chat messages are parsed into a 'user' and 'msg' string and
+        then passed to the method 'handle_chat_message(user, msg)'
+
+        :param line: str, message from server to be parsed
+        """
+        if "PRIVMSG" not in line:
+            ping = self.check_for_ping(line)
+            if not ping:
+                print(line)
+
+        else:
+            user = line.split("!")[0][1:]
+            delimiter = "PRIVMSG {} :".format(self.channel)
+            line = line.split(delimiter)
+            msg = delimiter.join(line[1:])
+
+            self.handle_chat_message(user, msg)
 
     def check_for_ping(self, line):
+        """
+        Checks a server message to determine if it contains a
+        PING from the server, and then sends the appropriate
+        response to prevent a connection timeout.
+
+        :param line: str, message from the server
+        :return: bool, True if a PING message has been
+            received, otherwise False
+        """
         ping = line == "PING :tmi.twitch.tv"
         if ping:
             self.send(line.replace("PING", "PONG"))
@@ -95,37 +217,40 @@ class TwitchChat:
 
         return ping
 
-    def print_line(self, line):
-        if "PRIVMSG" not in line:
-            print(line)
+    def handle_chat_message(self, user, msg):
+        """
+        Passes the chat message to the 'print_chat_message'
+        method for formatting as well as the 'bot' object's
+        'handle_message()' method if one is set.
 
-        else:
-            user = line.split("!")[0][1:]
-            delimiter = "PRIVMSG {} :".format(self.channel)
-            line = line.split(delimiter)
-            msg = delimiter.join(line[1:])
-            self.handle_message(user, msg)
-
-    @staticmethod
-    def print_chat_message(user, msg):
-        msg = msg.replace("\r", "")
-        print("{:>25}: {}".format(user, msg))
-
-    def handle_message(self, user, msg):
+        :param user: str, name of user who sent the message
+        :param msg: str, message sent to the chat
+        """
         self.print_chat_message(user, msg)
 
         if self.bot:
             self.bot.handle_message(user, msg)
 
+    @staticmethod
+    def print_chat_message(user, msg):
+        """
+        Formats a chat message and prints to the console
+
+        :param user: str, user name
+        :param msg: str, message sent to chat
+        """
+        msg = msg.replace("\r", "")
+        print("{:>25}: {}".format(user, msg))
+
 
 if __name__ == "__main__":
     chat = TwitchChat(
-        "your_bot_name",
+        "ahp_helper_bot",
         "oauth.token",
-        "#your_twitch_name"
+        "#athenshorseparty420"
     )
 
-    chat.join_chat()
+    chat.join_chat("I have joined the chat!")
 
     while True:
         chat.update_chat()
